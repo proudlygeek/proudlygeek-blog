13cf116bdd9595fdb050ddfb5986f2ab624b7896
g.bargelli <g.bargelli@gmail.com>
2011-01-22 19:44:27 +0100

    Added CSS (custom gist syntax highlight)

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -1,6 +1,32 @@
-/* Author: 
-
+/* Author: scroolose 
+http://got-ravings.blogspot.com/2010/06/line-numbers-in-embedded-gists.html
 */
+
+function addLineNumbersToAllGists() {
+  $('.gist').each( function() {
+      _addLineNumbersToGist('#' + $(this).attr('id'));
+  });
+}
+
+function addLineNumbersToGist(id) {
+  _addLineNumbersToGist('#gist-' + id);
+}
+
+function _addLineNumbersToGist(css_selector) {
+  $(document).ready( function() {
+    $(css_selector + ' .line').each(function(i, e) {
+      $(this).prepend(
+        $('<div/>').css({
+          'float' : 'left',
+          'width': '30px',
+          'font-weight' : 'bold',
+          'color': '#808080'
+        }).text(++i)
+      );
+    });
+  });
+}
+
 
 
 
